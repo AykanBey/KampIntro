73c296225fc978fa786a4bf33918a25899ae38e7
BuildTools <unconfigured@null.spigotmc.org>
2022-12-31 14:33:11 +0300

    Proje dosyası ekle.

---
+++ Donguler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = { "Yazılım Geliştirici Yetiştirme Kampı" , "Programlamaya başlangıç için temel kurs" , "Java" , "Python" , "C#" , "C++" };
+
+            for (int i = 0; i<kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For Burada bitiyor");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+
+            }
+
+            Console.WriteLine("Sayfa Sonu - Footer");
+
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya Elması";
+
+            Console.WriteLine("Hello World!");
+
+            Urun urun1 = new Urun[] ();
+        }
+    }
+}
