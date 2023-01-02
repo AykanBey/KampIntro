859cfad950c51118531534291f08f7854f69ffe4
BuildTools <unconfigured@null.spigotmc.org>
2023-01-02 16:14:52 +0300

    3. Gün Dersleri bitti

--- Metotlar/Program.cs
+++ Metotlar/Program.cs
@@ -14,9 +14,40 @@ namespace Metotlar
             double fiyati = 15;
             string aciklama = "Amasya Elması";
 
-            Console.WriteLine("Hello World!");
+            
 
-            Urun urun1 = new Urun[] ();
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("------------");
+
+            }
+
+            Console.WriteLine("--------Metotlar----------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12);
+            sepetManager.Ekle2("Elma", "Kırmızı Elma", 12);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır Karpuzu", 12);
+
+
         }
     }
 }
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+
+            Console.WriteLine("Sepete Eklendi. Sepete Eklenen ürün: " + urun.Adi);
+
+        }
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int StokAdedi) 
+        {
+        
+        Console.WriteLine("Sepete Eklendi. Sepete Eklenen ürün:" + urunAdi + "," + aciklama + "," + fiyat);
+
+        }
+    }
+}
+
